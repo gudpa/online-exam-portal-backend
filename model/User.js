9208c217777d0f96c0199819bca3077b7ba877bd
Alok Shukla <14alokshukla@gmail.com>
2024-05-07 17:46:47 +0530

    Added signup big form

--- model/User.js
+++ model/User.js
@@ -23,6 +23,45 @@ const UserSchema = new mongoose.Schema(
     branch: {
       type: String,
     },
+    fathersname: {
+      type: String,
+    },
+    mothersname: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+    dob: {
+      type: String,
+    },
+    religion: {
+      type: String,
+    },
+    motherTounge: {
+      type: String,
+    },
+    familyIncome: {
+      type: String,
+    },
+    nationality: {
+      type: String,
+    },
+    addressLine1: {
+      type: String,
+    },
+    addressLine2: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    taluka: {
+      type: String,
+    },
+    pincode: {
+      type: String,
+    },
     reset: {
       type: Boolean,
     },
